b840a6ecedd15d6b150a4ac9013eab0a1b605e4e
snowlight-aemt <it.s.aemt@gmail.com>
2023-04-27 23:20:04 +0900

    결제 준비 API

---
+++ spring-kakao-pay-api/src/main/java/me/snowlight/springkakaopayapi/controller/KakaoTokenRequest.java
@@ -0,0 +1,8 @@
+package me.snowlight.springkakaopayapi.controller;
+
+import lombok.Data;
+
+@Data
+public class KakaoTokenRequest {
+    private String pg_token;
+}
--- spring-kakao-pay-api/src/main/java/me/snowlight/springkakaopayapi/controller/PaymentApiController.java
+++ spring-kakao-pay-api/src/main/java/me/snowlight/springkakaopayapi/controller/PaymentApiController.java
@@ -12,12 +12,10 @@ import org.springframework.http.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.Mapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -31,9 +29,49 @@ public class PaymentApiController {
     @Value("${kakao.api.key}")
     private String key;
 
-    @GetMapping("/")
-    public ResponseEntity<String> root() {
-        log.info("ROOT");
+    @GetMapping("/success")
+    public ResponseEntity<String> success(@RequestParam("pg_token") String pg_token) {
+        log.info("success");
+        log.info(pg_token);
+//
+//        PaymentDto.KakaoReadyRequest readyRequest = PaymentDto.KakaoReadyRequest.builder()
+//                .cid("TC0ONETIME")
+//                .partner_order_id("122222")
+//                .partner_user_id("1122333")
+//                .item_name("TTTTTT")
+//                .quantity("1")
+//                .total_amount("2200")
+////                .vat_amount("200")
+//                .tax_free_amount("0")
+//                .approval_url("http://localhost:8080/success")
+//                .fail_url("http://localhost:8080/fail")
+//                .cancel_url("http://localhost:8080/cancel")
+//                .build();
+//
+//        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+//        params.setAll(objectMapper.convertValue(readyRequest, new TypeReference<>() {}));
+//
+//        HttpHeaders headers = new HttpHeaders();
+//        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+//        headers.setAccept(List.of(MediaType.APPLICATION_JSON));
+//        headers.set("Authorization", "KakaoAK " + key);
+//
+//        HttpEntity<MultiValueMap<String, String>> body = new HttpEntity<>(params, headers);
+//        ResponseEntity<String> exchange = restTemplate.exchange("https://kapi.kakao.com/v1/payment/ready", HttpMethod.POST, body, String.class);
+
+        return ResponseEntity.ok("OK");
+    }
+    @GetMapping("/fail")
+    public ResponseEntity<String> fail(@RequestParam Map<String, String> params) {
+        log.info("fail");
+        log.info(params.toString());
+        log.info(this.tid);
+        return ResponseEntity.ok("OK");
+    }
+    @GetMapping("/cancel")
+    public ResponseEntity<String> cancel(@RequestParam Map<String, String> params) {
+        log.info("cancel");
+        log.info(params.toString());
         return ResponseEntity.ok("OK");
     }
 
@@ -48,9 +86,9 @@ public class PaymentApiController {
                 .total_amount("2200")
 //                .vat_amount("200")
                 .tax_free_amount("0")
-                .approval_url("http://localhost:8080/")
-                .fail_url("http://localhost:8080/")
-                .cancel_url("http://localhost:8080/")
+                .approval_url("http://localhost:8080/success")
+                .fail_url("http://localhost:8080/fail")
+                .cancel_url("http://localhost:8080/cancel")
                 .build();
 
         MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
@@ -64,6 +102,9 @@ public class PaymentApiController {
         HttpEntity<MultiValueMap<String, String>> body = new HttpEntity<>(params, headers);
         ResponseEntity<String> exchange = restTemplate.exchange("https://kapi.kakao.com/v1/payment/ready", HttpMethod.POST, body, String.class);
         log.info(exchange.getBody().toString());
+
+        HashMap<String, String> hashMap = this.objectMapper.readValue(exchange.getBody(), HashMap.class);
+
         return exchange;
     }
 }
