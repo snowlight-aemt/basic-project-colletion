f390c7ff15d03d828d2333c1bfa8f71e5be083b1
snowlight-aemt <it.s.aemt@gmail.com>
2023-11-25 13:32:04 +0900

    Kotlin | Gradle Kts 적용

--- fastcampus-todo-java-master/build.gradle.kts
+++ fastcampus-todo-java-master/build.gradle.kts
@@ -12,6 +12,12 @@ group = "com.fastcampus.kotlinspring"
 version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_11
 
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
 repositories {
     mavenCentral()
 }
@@ -19,13 +25,14 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
-
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-
+    compileOnly("org.projectlombok:lombok")
     runtimeOnly("com.h2database:h2")
+    annotationProcessor("org.projectlombok:lombok")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("com.ninja-squad:springmockk:3.1.1")
 
 }
 
