73cb1a8a7e3350e1306c938e25d6cc5ed1ecd1fa
snowlight-aemt <it.s.aemt@gmail.com>
2023-11-25 11:37:07 +0900

    Kotlin Class 의 편의 플러그인 추가

--- spring-kotlin/build.gradle.kts
+++ spring-kotlin/build.gradle.kts
@@ -3,13 +3,28 @@ plugins {
 	id("org.springframework.boot") version "3.2.0"
 	id("io.spring.dependency-management") version "1.1.4"
 	kotlin("jvm") version "1.9.21"
-	id("org.jetbrains.kotlin.plugin.allopen") version "1.9.21"
+//	// TODO class 의 지정한 클래스가 상속이 가능하도록 해주는 플러그인 (상속)
+//	id("org.jetbrains.kotlin.plugin.allopen") version "1.9.21"
+//	// TODO Class 에 기본 생성자를 추가해주는 플러그인 (생성자)
+//	kotlin("plugin.noarg") version "1.9.21"
+
+	// TODO Spring 관련 모든 애노테이션을 추가해주는 플러그인
+	kotlin("plugin.spring") version "1.9.21"
+	// TODO JPA 관련 모든 애노테이션을 추가해주는 플러그인
+	kotlin("plugin.jpa") version "1.9.21"
 }
 
-// 명시한 어노테이션이 붙어 있는 모든 클래스에 open 를 붙여준다. (상속)
-allOpen {
-	annotations("org.springframework.boot.autoconfigure.SpringBootApplication")
-}
+
+// TODO 명시한 어노테이션이 붙어 있는 모든 클래스에 open 를 붙여준다. (상속)
+//allOpen {
+//	annotations("org.springframework.boot.autoconfigure.SpringBootApplication")
+//}
+
+// TODO 명시한 어노테이션이 붙어 있는 모든 클래스에 기본 생성자를 추가해 준다. (생성자)
+//noArg {
+//	annotations("jakarta.persistence.Entity")
+//}
+
 
 group = "me.snowlight"
 version = "0.0.1-SNAPSHOT"
--- spring-kotlin/src/main/java/me/snowlight/springkotlin/SpringKotlinApplication.kt
+++ spring-kotlin/src/main/java/me/snowlight/springkotlin/SpringKotlinApplication.kt
@@ -1,7 +1,10 @@
 package me.snowlight.springkotlin
 
+import jakarta.persistence.*
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.transaction.annotation.Transactional
+import javax.annotation.processing.Generated
 
 @SpringBootApplication
 class SpringKotlinApplication
@@ -9,3 +12,20 @@ class SpringKotlinApplication
 fun main(args: Array<String>) {
     runApplication<SpringKotlinApplication>()
 }
+
+@Transactional
+class Service
+
+@Entity
+@Table
+class Person (
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id : Long?,
+
+    @Column
+    var name : String,
+
+    @Column
+    var age : Int,
+)
