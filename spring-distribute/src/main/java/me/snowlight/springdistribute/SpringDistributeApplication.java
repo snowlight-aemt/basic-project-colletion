c7dac741a05f2ea1a8a64576ef641f2495d589bd
snowlight-aemt <it.s.aemt@gmail.com>
2024-03-30 09:41:53 +0900

    DiSTRIBUTE - INIT

---
+++ spring-distribute/src/main/java/me/snowlight/springdistribute/SpringDistributeApplication.java
@@ -0,0 +1,13 @@
+package me.snowlight.springdistribute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDistributeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDistributeApplication.class, args);
+    }
+
+}
