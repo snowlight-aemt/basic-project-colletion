834ab810fed7c7f1c32058a1b97d3d9ef2e50302
snowlight-aemt <it.s.aemt@gmail.com>
2024-08-12 23:12:15 +0900

    Models 적용

--- django_pylog/pylog/models.py
+++ django_pylog/pylog/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+class Blog(models.Model):
+	blog_no = models.IntegerField(primary_key=True)
+	title = models.CharField(max_length=100)
+	email = models.EmailField()
+	body = models.TextField()
+	createdAt = models.DateTimeField(auto_now_add=True)
+	updatedAt = models.DateTimeField(auto_now=True)
+
+
+class Comments(models.Model):
+	blog_no = models.ForeignKey(Blog, on_delete=models.CASCADE)
+	userId = models.CharField(max_length=100)
+	comment = models.TextField()
+	createdAt = models.DateTimeField(auto_now_add=True)
+	updatedAt = models.DateTimeField(auto_now=True)
+
+
+class Tags(models.Model):
+	tag_name = models.CharField(max_length=100).unique
+	createdAt = models.DateTimeField(auto_now_add=True)
+	updatedAt = models.DateTimeField(auto_now=True)
+	
+
+class BlogTag(models.Model):
+	blog_no = models.ForeignKey(Blog, on_delete=models.CASCADE)
+	tag_name = models.ForeignKey(Tags, on_delete=models.CASCADE)
+
+
