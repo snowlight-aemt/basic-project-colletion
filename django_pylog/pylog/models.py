99f31297469ccf3c28364b541ed1c25814fa14ba
snowlight-aemt <it.s.aemt@gmail.com>
2024-08-15 21:34:49 +0900

    blog 생성, 조회 & DB Models 수정

--- django_pylog/pylog/models.py
+++ django_pylog/pylog/models.py
@@ -1,35 +1,32 @@
-from datetime import timezone
-from email.policy import default
-
 from django.db import models
 
 
 class Blog(models.Model):
-	blog_no = models.IntegerField(primary_key=True, unique=True, null=False, blank=False, default=0)
-	title = models.CharField(max_length=100)
-	email = models.EmailField()
-	content = models.TextField()
-	createdAt = models.DateTimeField(auto_now_add=True)
-	updatedAt = models.DateTimeField(auto_now=True)
+    blog_no = models.BigAutoField(primary_key=True)
+    title = models.CharField(max_length=100)
+    email = models.EmailField()
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
 
 class Comments(models.Model):
-	comment_no = models.IntegerField(primary_key=True, unique=True, null=False, blank=False, default=0)
-	blog_no = models.ForeignKey(Blog, on_delete=models.CASCADE)
-	userId = models.CharField(max_length=100)
-	comment = models.TextField()
-	createdAt = models.DateTimeField(auto_now_add=True)
-	updatedAt = models.DateTimeField(auto_now=True)
+    comment_no = models.BigAutoField(primary_key=True)
+    blog_no = models.ForeignKey(Blog, on_delete=models.CASCADE)
+    userId = models.CharField(max_length=100)
+    comment = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
 
 class Tags(models.Model):
-	tag_no = models.IntegerField(primary_key=True, unique=True, null=False, blank=False, default=0)
-	tag_name = models.CharField(max_length=100, unique=True, null=False, blank=False, default=0)
-	createdAt = models.DateTimeField(auto_now_add=True)
-	updatedAt = models.DateTimeField(auto_now=True)
+    tag_no = models.BigAutoField(primary_key=True)
+    tag_name = models.CharField(max_length=100, unique=True, null=False, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
 
 class BlogTag(models.Model):
-	blog_tag_no = models.IntegerField(primary_key=True, unique=True, null=False, blank=False, default=0)
-	blog_no = models.ForeignKey(Blog, on_delete=models.CASCADE)
-	tag_no = models.ForeignKey(Tags, on_delete=models.CASCADE)
+    blog_tag_no = models.BigAutoField(primary_key=True)
+    blog_no = models.ForeignKey(Blog, on_delete=models.CASCADE)
+    tag_no = models.ForeignKey(Tags, on_delete=models.CASCADE)
--- django_pylog/pylog/views.py
+++ django_pylog/pylog/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from tempfile import template
 
-# Create your views here.
+from django.http import HttpResponse, HttpRequest
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import CreateView
+
+from pylog.forms import BlogForm
+from pylog.models import Blog
+
+
+def blog(request: HttpRequest) -> HttpResponse:
+    if request.GET:
+        blog_list = Blog.objects.all()
+
+        return render(request, "pylog/blog.html", {"post_list": blog_list})
+    elif request.POST:
+        # print(request.POST.get("content"))
+
+        Blog.objects.create(
+            title=request.POST["title"],
+            content=request.POST["content"],
+            email=request.POST["email"],
+        )
+
+    return render(request, "pylog/blog.html", {})
+
+
+def blog_detail(request: HttpRequest, blog_no: int) -> HttpResponse:
+    blog = get_object_or_404(Blog, pk=blog_no)
+
+    return render(request, "pylog/blog_detail.html", {"post": blog})
+
+
+blog_new = CreateView.as_view(model=Blog, form_class=BlogForm, success_url="/success")
