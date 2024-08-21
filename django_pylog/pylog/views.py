029111d69539e4a9ad589517b1f2dc29697b6afb
snowlight-aemt <it.s.aemt@gmail.com>
2024-08-21 23:12:27 +0900

    ADD 화면 레이어 구성

--- django_pylog/pylog/views.py
+++ django_pylog/pylog/views.py
@@ -1,5 +1,6 @@
 from tempfile import template
 
+from django.forms import model_to_dict
 from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import CreateView
@@ -13,11 +14,16 @@ def blog(request: HttpRequest) -> HttpResponse:
         title = request.GET.get("title")
 
         if title is not None:
-            blog_list = Blog.objects.filter(title=title)
+            new_blog_list = Blog.objects.filter(title=title)
         else:
             blog_list = Blog.objects.all()
+            new_blog_list = list()
+            for item in blog_list:
+                blog_dict = model_to_dict(item)
+                blog_dict["summary"] = blog_dict["content"][0:100]
+                new_blog_list.append(blog_dict)
 
-        context = {"post_list": blog_list}
+        context = {"post_list": new_blog_list}
 
         return render(request, "pylog/blog.html", context)
     elif request.method == "POST":
